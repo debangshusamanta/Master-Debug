3415735fa282a9e753212a68bc12db5a32a8bcad
Debangshu <debangshu.dev001@gmail.com>
2025-07-19 21:31:25 +0530

    Fixed

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -8,7 +8,7 @@ import dotenv from 'dotenv';
 
 const app = express();
 dotenv.config();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 const allowedOrigins = [
   'https://master-debug.vercel.app',
